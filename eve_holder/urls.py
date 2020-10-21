fa64a06ee52dd612c53c0e1c66d692513038e626
james31366 <james31366@gmail.com>
2020-10-21 13:03:19 +0700

    can use edit and delete button

--- eve_holder/urls.py
+++ eve_holder/urls.py
@@ -7,5 +7,10 @@ app_name = 'eve_holder'
 urlpatterns = [
     path('', views.home, name='home'),
     path('events/', views.events, name='events'),
-    path('visitor/<str:pk>/', views.visitor, name='visitor')
+    path('visitor/<str:pk>/', views.visitor, name='visitor'),
+
+    path('create_event/', views.create_event, name='create_event'),
+    path('edit_event/<str:pk>/', views.edit_event, name='edit_event'),
+    path('delete_event/<str:pk>/', views.delete_event, name='delete_event')
+
 ]
--- eve_holder/views.py
+++ eve_holder/views.py
@@ -1,6 +1,7 @@
 """This module contains views of the website."""
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from .forms import EventForm
 from .models import *
 
 
@@ -28,3 +29,39 @@ def visitor(request, pk):
 
     context = {'visitors': visitors_list, 'events': event_list, 'event_count': event_count}
     return render(request, 'eve_holder/visitor.html', context)
+
+
+def create_event(request):
+    form = EventForm()
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'eve_holder/event_form.html', context)
+
+
+def edit_event(request, pk):
+    events_list = Event.objects.get(id=pk)
+    form = EventForm(instance=events_list)
+
+    if request.method == 'POST':
+        form = EventForm(request.POST, instance=events_list)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context = {'form': form}
+    return render(request, 'eve_holder/event_form.html', context)
+
+
+def delete_event(request, pk):
+    events_list = Event.objects.get(id=pk)
+    if request.method == 'POST':
+        events_list.delete()
+        return redirect('/')
+
+    context = {'item': events_list}
+    return render(request, 'eve_holder/delete.html', context)
