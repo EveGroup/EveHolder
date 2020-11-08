aed88f4b72ddc787a2a86e7a582beeb2bbad49bc
james31366 <james31366@gmail.com>
2020-11-08 23:27:40 +0700

    add test_event_name

--- eve_holder/tests/test_event_models.py
+++ eve_holder/tests/test_event_models.py
@@ -23,14 +23,14 @@ class EventModelTest(TestCase):
     def test_can_register_in_register_date(self):
         pub_date = timezone.now() - datetime.timedelta(days=1)
         end_date = pub_date + datetime.timedelta(days=2)
-        future_event = Event(event_name="Future Event", pub_date=pub_date, end_date=end_date)
-        self.assertTrue(future_event.can_register())
+        register_event = Event(event_name="Register Event", pub_date=pub_date, end_date=end_date)
+        self.assertTrue(register_event.can_register())
 
     def test_can_register_after_end_date(self):
         pub_date = timezone.now() - datetime.timedelta(days=1)
         end_date = pub_date + datetime.timedelta(hours=20)
-        future_event = Event(event_name="Future Event", pub_date=pub_date, end_date=end_date)
-        self.assertFalse(future_event.can_register())
+        old_event = Event(event_name="Old Event", pub_date=pub_date, end_date=end_date)
+        self.assertFalse(old_event.can_register())
 
     def test_is_expired_before_pubdate(self):
         pub_date = timezone.now() + datetime.timedelta(days=1)
@@ -41,14 +41,17 @@ class EventModelTest(TestCase):
     def test_is_expired_in_register_date(self):
         pub_date = timezone.now() - datetime.timedelta(days=1)
         end_date = pub_date + datetime.timedelta(days=2)
-        future_event = Event(event_name="Future Event", pub_date=pub_date, end_date=end_date)
-        self.assertFalse(future_event.is_expired())
+        register_event = Event(event_name="Register Event", pub_date=pub_date, end_date=end_date)
+        self.assertFalse(register_event.is_expired())
 
     def test_is_expired_after_end_date(self):
         pub_date = timezone.now() - datetime.timedelta(days=1)
         end_date = pub_date + datetime.timedelta(hours=20)
-        future_event = Event(event_name="Future Event", pub_date=pub_date, end_date=end_date)
-        self.assertTrue(future_event.is_expired())
+        old_event = Event(event_name="Old Event", pub_date=pub_date, end_date=end_date)
+        self.assertTrue(old_event.is_expired())
 
     def test_event_name(self):
-        pub_date = 
+        pub_date = timezone.now() - datetime.timedelta(days=1)
+        end_date = pub_date + datetime.timedelta(days=2)
+        register_event = Event(event_name="Register Event", pub_date=pub_date, end_date=end_date)
+        self.assertEqual(str(register_event), "Register Event")
