cb1a735798df5b5448f43a69a236b8f744305acc
james31366 <james31366@gmail.com>
2020-11-20 00:27:15 +0700

    delete .travis.yml change installed_apps config in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -33,7 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'eve_holder',
+    'eve_holder.apps.EveHolderConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
