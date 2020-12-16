b703a34d158b5f66693f9d91bc056af6034c3c5e
Vichisorn <james31366@gmail.com>
2020-12-17 00:28:48 +0700

    uncomment django security middleware

--- mysite/settings.py
+++ mysite/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    # 'django.middleware.security.SecurityMiddleware',
+    'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
